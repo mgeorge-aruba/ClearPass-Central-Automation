33ddda45796d8d4a0d6ca6c0be0af855419f9d2d
mgeorge-aruba <105308214+mgeorge-aruba@users.noreply.github.com>
2024-01-06 13:43:42 -0800

    Create credsConfig-SAMPLE.py

---
+++ credsConfig-SAMPLE.py
@@ -0,0 +1,14 @@
+
+#This is a sample credentials file. Original file should be imported in createNAD.py script
+
+clientid = "CPPM-API-Client"
+clientsecret = "fake-secretxuf+EL6Z5Qi3ekkPhj"
+
+central_info = {
+            "username": "user@domain.com",
+            "password": "my-central-password",
+            "client_id": "fake-client-idH6SgBjJpXuc9qGe5DO65",
+            "client_secret": "fake-client-secrettU41rSIH31i1a5ynf6LZ",
+            "customer_id": "fake-customer-id1eda5a662fd41b37222",
+            "base_url": "https://uswest4-apigw.central.arubanetworks.com/swagger/apps/nms/"
+        }
